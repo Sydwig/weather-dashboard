32e498e48378ec0ac1f339a87ba4a714b0b746fb
Exotic P <sjwiegman@gmail.com>
2022-08-29 23:51:33 -0400

    allowed duplicates in localstorage to be deleted

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var toJSON = function (response) { //response toJSON coversion
     return response.json();
 };
 
-var DisplayWeather = function (data, city) { 
+var DisplayWeather = function (data, city) {
     console.log(data);
     var fiveDayEl = document.querySelector('#fiveDay')
     var currentEl = document.querySelector('#current');
@@ -18,7 +18,7 @@ var DisplayWeather = function (data, city) {
     tempEl.textContent = 'TEMP: ' + data.current.temp;
     currentEl.appendChild(h2El);
     currentEl.appendChild(tempEl);
-    var fiveDay = data.daily.slice(1,6);
+    var fiveDay = data.daily.slice(1, 6);
     fiveDayEl.innerHTML = null;
     for (var day of fiveDay) {
         var date = new Date(1661878800 * 1000).toLocaleDateString();
@@ -29,8 +29,8 @@ var DisplayWeather = function (data, city) {
         cardEl.className = "card p-3 m-3";
         var dateEl = document.createElement('p');
         var tempEl = document.createElement('p');
-        var imgEl = document.createElement('img'); 
-        var icon = day.weather[0].icon; 
+        var imgEl = document.createElement('img');
+        var icon = day.weather[0].icon;
         imgEl.alt = icon;
         imgEl.width = 90;
         imgEl.height = 90;
@@ -42,7 +42,7 @@ var DisplayWeather = function (data, city) {
         cardEl.append(tempEl);
         cardEl.append(imgEl);
         cardEl.append(dateEl);
-    }   
+    }
 };
 
 var getOneCall = function (city) {
@@ -57,12 +57,13 @@ var getOneCall = function (city) {
 var saveToLocalStorage = function (city) { //saves entry to local storage
     var cities = JSON.parse(localStorage.getItem('cities')) || [];
     cities.push(city);
-    var data = JSON.stringify(cities);
+    var citySet = Array.from(new Set(names));
+    var data = JSON.stringify(citySet);
     localStorage.setItem('cities', data);
     displayButtons();
 };
 
-var getGeo = function (locations) {
+var getGeo = function (locations) { //gets and saves city info
     var city = locations[0];
     console.log('LAT', city.lat);
     console.log('LON', city.lon);
@@ -70,7 +71,7 @@ var getGeo = function (locations) {
     getOneCall(city);
 };
 
-var displayButtons = function () {
+var displayButtons = function () { //displays previously stored cities
     var cities = JSON.parse(localStorage.getItem('cities')) || [];
     var showOnlyFiveCities = cities.slice(cities.length - 5);
     searchedCities.innerHTML = null;
@@ -82,7 +83,7 @@ var displayButtons = function () {
     }
 };
 
-var handleCityClick = function (event) {
+var handleCityClick = function (event) { //selects locally stored city
     event.preventDefault();
     if (event.target.matches('button')) {
         var q = event.target.textContent;
@@ -93,7 +94,7 @@ var handleCityClick = function (event) {
     }
 };
 
-var runSearch = function(event) {
+var runSearch = function (event) { //search for new city
     event.preventDefault();
     var q = document.querySelector('#q');
     var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q.value}&appid=${appid}`;
@@ -104,4 +105,4 @@ var runSearch = function(event) {
 
 searchEl.addEventListener('click', runSearch);
 searchedCities.addEventListener('click', handleCityClick);
-displayButtons();
+displayButtons(); //displays previous searches on page refresh

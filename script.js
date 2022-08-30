d37a6614c0c6013e42226a6c6fa77f94ca17f26f
Exotic P <sjwiegman@gmail.com>
2022-08-29 21:18:06 -0400

    added geoURL fetch in javascript

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 var appId = '2bc993b295d6552601151ddd85bc5a4a';
-var q = 'Chicago';
-var baseURL = `http://api.openweathermap.org/geo/1.0/direct?q=Chicago&appid=2bc993b295d6552601151ddd85bc5a4a`;
+var q = 'Chicago'; //this will be an input soon
+var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appId}`; //open weather API here
+
+fetch(geoURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (locations) {
+      console.log(locations);
+      var city = locations[0];
+      console.log('LAT', city.lat);
+      console.log('LON', city.lon);
+    });
+    

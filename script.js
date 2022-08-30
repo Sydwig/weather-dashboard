2e1037cffe8f74428387c314fe62c314e15ae869
Exotic P <sjwiegman@gmail.com>
2022-08-29 22:16:14 -0400

    fixed javascript error 401 and added HTML elements for a search form

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
-    var appid = '485bbc753e29e9770f09ca55c32c6d79'; //had to use Anthony's API key
-    var q = 'Chicago'; //currently loggin Chicago for testing
-    var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appid}`;
+//global variables
+var appid = '485bbc753e29e9770f09ca55c32c6d79'; //had to use Anthony's API key
+var q = 'Chicago'; //currently loggin Chicago for testing
+var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appid}`;
 
-    fetch(geoURL)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (locations) {
-        var city = locations[0];
-        console.log('LAT', city.lat);
-        console.log('LON', city.lon);
-        var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.lon}&appid=${appid}&units=imperial&exclude=hourly,minutely`;
-        fetch(oneCall)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (data) {
-            console.log(data);
-            var h2El = document.createElement('h2');
-            var tempEl = document.createElement('p');
-            h2El.textContent = city.name;
-            tempEl.textContent = 'TEMP: ' + data.current.temp;
-            document.body.appendChild(h2El);
-            document.body.appendChild(tempEl);
-          });
-      });
+var toJSON = function (response) { //response toJSON coversion
+    return response.json();
+};
 
-      var saveToLocalStorage = function(city) { //saves entry to local storage
-        var cities = JSON.parse(localStorage.getItem('cities')) || [];
-        cities.push(city);
-        var data = JSON.stringify(cities);
-        localStorage.setItem('cities', data);
-      };
+var DisplayWeather = function (data, city) { 
+    console.log(data);
+    var h2El = document.createElement('h2');
+    var tempEl = document.createElement('p');
+    h2El.textContent = city.name;
+    tempEl.textContent = 'TEMP: ' + data.current.temp;
+    document.body.appendChild(h2El);
+    document.body.appendChild(tempEl);
+};
 
+var getOneCall = function (city) {
+    var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.lon}&appid=${appid}&units=imperial&exclude=hourly,minutely`;
+    fetch(oneCall)
+        .then(toJSON)
+        .then(function(data){
+            DisplayWeather(data, city);
+        });
+};
+
+
+var getGeo = function (locations) {
+    var city = locations[0];
+    console.log('LAT', city.lat);
+    console.log('LON', city.lon);
+    getOneCall(city);
+};
+
+var saveToLocalStorage = function (city) { //saves entry to local storage
+    var cities = JSON.parse(localStorage.getItem('cities')) || [];
+    cities.push(city);
+    var data = JSON.stringify(cities);
+    localStorage.setItem('cities', data);
+};
+
+fetch(geoURL)
+    .then(toJSON)
+    .then(getGeo);
